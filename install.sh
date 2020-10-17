af2460785f3d558607e756d2da7a35b6ead8f7f8
Dmitri Popov <dmpop@linux.com>
2020-10-17 16:55:30 +0200

    Switch to Linux Mint, Nemo

--- install.sh
+++ install.sh
@@ -12,18 +12,18 @@ dist=$(lsb_release -i | cut -f 2)
 
 if [ "$dist" == "openSUSE" ]; then
     sudo zypper up
-    sudo zypper install -y git jhead dcraw xclip bc jq ImageMagick exiftool libnotify-tools
+    sudo zypper install -y git jhead xclip bc jq ImageMagick exiftool libnotify-tools
     sudo mkdir -p /usr/share/kservices5/ServiceMenus
     sudo mkdir -p /usr/share/icons/konbini-icons
     sudo cp -R icons/*.png /usr/share/icons/konbini-icons/
     sudo cp -R desktop/* /usr/share/kservices5/ServiceMenus/
     cp -R dolphin/* $HOME/bin/
-elif [ "$dist" == "Ubuntu" ]; then
+elif [ "$dist" == "Linuxmint" ]; then
     sudo apt-get update
     sudo apt-get upgrade
-    sudo apt-get -y install git jhead dcraw xclip bc jq imagemagick libimage-exiftool-perl libnotify-bin
-    mkdir -p $HOME/.local/share/nautilus/scripts/Konbini
-    cp nautilus/* $HOME/.local/share/nautilus/scripts/Konbini
+    sudo apt-get -y install git jhead bc jq imagemagick libimage-exiftool-perl libnotify-bin
+    mkdir -p $HOME/.local/share/nemo/scripts/Konbini
+    cp nemo/* $HOME/.local/share/nemo/scripts/Konbini
 else
     echo "Unsupported operating system."
     exit 1
